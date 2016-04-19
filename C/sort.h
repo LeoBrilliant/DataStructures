3995328f298fd1cc0c47c26e3a9a66dc680712fe
LeoBrilliant <LeoBrilliant@gmail.com>
2016-04-19 20:36:17 +0800

    some sort algorithym
    Signed-off-by: LeoBrilliant <LeoBrilliant@gmail.com>

--- C/sort.h
+++ C/sort.h
@@ -12,4 +12,10 @@ int issort(void *data, int size, int esize, int(*compare)(const void *key1, cons
 
 int qksort(void *data, int size, int esize, int i, int k, int(*compare)(const void *key1, const void *key2));
 
+int mgsort(void *data, int size, int esize, int i, int k, int(*compare)(const void *key1, const void *key2));
+
+int ctsort(int *data, int size, int k);
+
+int rxsort(int *data, int size, int p, int k);
+
 #endif /* C_SORT_H_ */
